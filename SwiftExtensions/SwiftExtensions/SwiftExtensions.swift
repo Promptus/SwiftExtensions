f72d0b7fab31913d79e08fa1eec80699a06285ec
Razvan Benga <razvanb@fortech.ro>
2015-11-03 18:35:45 +0100

    removed the extension header

--- SwiftExtensions/SwiftExtensions/SwiftExtensions.swift
+++ SwiftExtensions/SwiftExtensions/SwiftExtensions.swift
@@ -8,22 +8,12 @@
 
 import Foundation
 
-public class SwiftHelper {
-    
-    public var shit: String {
-        get {
-            return "Shit"
+public func filterString(string: String, predicate: Character -> Bool) -> String {
+    var result = String()
+    for character in string.characters {
+        if predicate(character) {
+            result.append(character)
         }
     }
-    
-    public func filterString(string: String, predicate: Character -> Bool) -> String {
-        var result = String()
-        for character in string.characters {
-            if predicate(character) {
-                result.append(character)
-            }
-        }
-        return result
-    }
-    
+    return result
 }
