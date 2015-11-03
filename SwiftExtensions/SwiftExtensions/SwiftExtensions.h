21648cf5b6afe741576a6f92232e51e8148d68a1
Razvan Benga <razvanb@fortech.ro>
2015-11-03 14:08:35 +0100

    changed the swiftextensions.h file

--- SwiftExtensions/SwiftExtensions/SwiftExtensions.h
+++ SwiftExtensions/SwiftExtensions/SwiftExtensions.h
@@ -15,5 +15,5 @@ FOUNDATION_EXPORT double SwiftExtensionsVersionNumber;
 FOUNDATION_EXPORT const unsigned char SwiftExtensionsVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SwiftExtensions/PublicHeader.h>
-
+#import <StringExtension.swift>
 
